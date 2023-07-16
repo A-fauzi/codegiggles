c613ebb48681e57882daf31843fb81042d9e51f2
“UA-fauzi <akhmadfauzi0101@gmail.com>
2023-07-16 20:08:23 +0000

    modify site.config.js, change google search console enable true

--- site.config.js
+++ site.config.js
@@ -51,7 +51,7 @@ const CONFIG = {
 
   },
   googleSearchConsole: {
-    enable: false,
+    enable: true,
     config: {
       siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
     },
