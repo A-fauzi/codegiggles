fb57d4518767a55c50a96b858492a83e2aa6774d
“UA-fauzi <akhmadfauzi0101@gmail.com>
2023-07-22 00:22:45 +0000

    modify navbar

--- src/components/Layout/Header/NavBar.tsx
+++ src/components/Layout/Header/NavBar.tsx
@@ -1,8 +1,8 @@
 'use client'
  
-import { useState } from 'react'
+// import { useState } from 'react'
 import Link from "next/link";
-import MobileNavModal from "./NavbarMobile";
+// import MobileNavModal from "./NavbarMobile";
 
 const NavBar: React.FC = () => {
   const links = [
@@ -11,9 +11,9 @@ const NavBar: React.FC = () => {
     { id: 3, name: "Privacy Police", to: "/privacy-police" },
   ];
 
-  const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+  // const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
 
-  const handleToggleMobileNav = () => {
+ // const handleToggleMobileNav = () => {
     setIsMobileNavOpen(!isMobileNavOpen);
   };
 
@@ -37,17 +37,7 @@ const NavBar: React.FC = () => {
 
       {/* Mobile Navbar */}
       <div className="block md:hidden">
-        <button
-          onClick={handleToggleMobileNav}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 rounded"
-        >
-          Menu
-        </button>
-        <MobileNavModal
-          isOpen={isMobileNavOpen}
-          onClose={() => setIsMobileNavOpen(false)}
-          links={links}
-        />
+        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Menu</button>
       </div>
     </>
   );
