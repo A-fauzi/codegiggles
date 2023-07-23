1039c425b58b035fbe836be9d6650d702102a568
“UA-fauzi <akhmadfauzi0101@gmail.com>
2023-07-23 06:18:47 +0000

    modify navbar

--- src/components/Layout/Header/NavBar.tsx
+++ src/components/Layout/Header/NavBar.tsx
@@ -37,7 +37,7 @@ const NavBar: React.FC = () => {
 
       {/* Mobile Navbar */}
       <div className="block md:hidden">
-        <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Menu</button>
+        <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-6 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Menu</button>
         
       </div>
     </>
